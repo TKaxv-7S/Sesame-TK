4842e3e963bbc2d533e381f3338df3886c4c9fd5
TKaxv_7S <954067342@qq.com>
2024-06-04 17:58:34 +0800

    测试 逻辑

--- app/src/main/java/pansong291/xposed/quickenergy/ui/MainActivity.java
+++ app/src/main/java/pansong291/xposed/quickenergy/ui/MainActivity.java
@@ -110,6 +110,7 @@ public class MainActivity extends Activity {
         }
         this.setTitle(this.getTitle() + version);
 
+        setModuleActive(isExpModuleActive(this));
         new AlertDialog.Builder(this)
                 .setTitle("提示")
                 .setMessage("本APP是为了学习研究开发，免费提供，不得进行任何形式的转发、发布、传播。请于24小时内卸载本APP。如果您是购买的可能已经被骗，请联系卖家退款。")
