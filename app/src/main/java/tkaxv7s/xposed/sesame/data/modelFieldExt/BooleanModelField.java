25136fb50bf9212218f183f18dcf172e047a567c
TKaxv_7S <954067342@qq.com>
2024-06-21 23:40:17 +0800

    修改 样式

--- app/src/main/java/tkaxv7s/xposed/sesame/data/modelFieldExt/BooleanModelField.java
+++ app/src/main/java/tkaxv7s/xposed/sesame/data/modelFieldExt/BooleanModelField.java
@@ -38,6 +38,7 @@ public class BooleanModelField extends ModelField {
         sw.setText(getName());
         sw.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         sw.setMinHeight(150);
+        sw.setMaxHeight(180);
         sw.setPaddingRelative(40, 0, 40, 0);
         sw.setChecked(getValue());
         sw.setOnClickListener(v -> setValue(((Switch) v).isChecked()));
