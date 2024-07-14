7c139cf83b68f83334763602ecf8bf7e5f5da90f
TKaxv_7S <954067342@qq.com>
2024-07-15 00:08:25 +0800

    修改 设置页UI

--- app/src/main/java/tkaxv7s/xposed/sesame/ui/MainActivity.java
+++ app/src/main/java/tkaxv7s/xposed/sesame/ui/MainActivity.java
@@ -2,7 +2,6 @@ package tkaxv7s.xposed.sesame.ui;
 
 import android.annotation.SuppressLint;
 import android.content.*;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
@@ -10,9 +9,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
-import android.widget.Button;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
